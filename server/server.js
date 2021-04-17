482a502bdfafec048b529ba474a66da04aed9528
Malachi <malachialusa@gmail.com>
2021-04-17 10:48:09 -0600

    removed logic

--- server/server.js
+++ server/server.js
@@ -33,10 +33,9 @@ app.get('*', (req, res) => {
 
 db.once('open', () => {
 
-  runseeds()
-
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
+
 });
